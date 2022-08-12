8a1ebfb9c7d9356f2f7064319679b8cad3e9b3a3
KarolinaGroszewska <karolina.groszewska@gmail.com>
2022-08-11 23:28:01 -0500

    login splash

--- App.tsx
+++ App.tsx
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, ImageBackground, Image, TouchableOpacity } from 'react-native';
+import AppLoading from "expo-app-loading";  
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+let customFonts = {
+  'Dongle-Bold': require('./assets/fonts/Dongle-Bold.ttf'),
+};
+
+export default class App extends React.Component {
+  state = {
+    fontsLoaded: false,
+  };
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading/>;
+    }
+    return (
+      <View>
+      <ImageBackground
+        source={require('./assets/background.png')}
+        style={styles.background}
+      >
+        <View>
+          <Image source={require('./assets/logo.png')} style={styles.logo} resizeMode="contain" >
+          </Image>
+          <View style={styles.buttons}>
+          <TouchableOpacity>
+            <Text style={styles.login}>Log In</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.login}>Sign Up</Text>
+          </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    background: {
+      width: '100%',
+      height: '100%'
+    },
+    logo:{
+      width: 280,
+      height: 280,
+      marginLeft: '17%',
+      marginTop: '55%'
+    },
+    login: {
+      overflow: 'hidden',
+      backgroundColor: '#FBB03B',
+      width: 125,
+      height: 40,
+      borderRadius: 7,
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontFamily: "Dongle-Bold",
+      color: "#D6860B",
+      padding: "5%",
+      fontSize: 20,
+    },
+    signup: {
+      overflow: 'hidden',
+      backgroundColor: '#FBB03B',
+      width: 125,
+      height: 40,
+      borderRadius: 7,
+      textAlign: 'center',
+      fontWeight: 'bold',
+      color: "#D6860B",
+      padding: "5%",
+      fontSize: 20,
+    },
+    buttons: {
+      flexDirection:"row", flex: 1, 
+      justifyContent: 'space-between', 
+      marginLeft: "17%", 
+      marginRight: "17%",
+      marginTop: "-10%",
+      }
 });
